e1cbe949c24da5d2fdc19b20401bffc306458b0a
Thorben Bender <bender.thorben0@gmail.com>
2019-05-29 16:03:19 +0200

    done with adventure game

--- src/adv.py
+++ src/adv.py
@@ -1,22 +1,23 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room("Outside",
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("Narrow", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -38,14 +39,57 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player1 = Player('Simon', room['outside']);
+
+if (player1.currentRoom == 'n_to'):
+    print('It is true')
 
 # Write a loop that:
+while True:
 #
 # * Prints the current room name
+    print(player1.currentRoom.name)
 # * Prints the current description (the textwrap module might be useful here).
+    print(player1.currentRoom.description)
 # * Waits for user input and decides what to do.
-#
+    print("You have 4 options where you can head to! [Nord]: n, [East]: e, [South]: south, [West]: west")
+    playerChoice = input('In which direction do you want to go: ')
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
+    # try:
+    #     if(playerChoice == '')
 #
 # If the user enters "q", quit the game.
+    try:
+        if (playerChoice == 'n'):
+            try:
+                player1.currentRoom.n_to
+                player1.moveRoom(room[player1.currentRoom.n_to.name.lower()])
+            except AttributeError: 
+                    player1.currentRoom.n_to = None
+                    print('This direction is invalid!')
+        elif(playerChoice == 'e'):
+            try:
+                player1.currentRoom.e_to
+                player1.moveRoom(room[player1.currentRoom.e_to.name.lower()])
+            except AttributeError: 
+                    player1.currentRoom.e_to = None
+                    print('This direction is invalid!')
+        elif(playerChoice == 's'):
+            try:
+                player1.currentRoom.s_to
+                player1.moveRoom(room[player1.currentRoom.s_to.name.lower()])
+            except AttributeError: 
+                    player1.currentRoom.s_to = None
+                    print('This direction is invalid!')
+        elif(playerChoice == 'w'):
+            try:
+                player1.currentRoom.w_to
+                player1.moveRoom(room[player1.currentRoom.w_to.name.lower()])
+            except AttributeError: 
+                    player1.currentRoom.w_to = None
+                    print('This direction is invalid!')
+        else:
+            break
+    except ValueError: 
+        break
--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, currentRoom):
+        self.name = name
+        self.currentRoom = currentRoom
+
+    def __str__(self):
+        return f'Your name is {self.name}. The room is {self.currentRoom.name} and {self.currentRoom.description}'
+
+    def moveRoom(self, direction):
+        self.currentRoom = direction
